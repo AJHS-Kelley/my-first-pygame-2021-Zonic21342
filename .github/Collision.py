9f3ad1cd385ca40dc036c307acf42025cc61673d
Zonic21342 <baconbruns@gmail.com>
2022-01-27 09:37:30 -0500

    V1.0
    Wrote code to move player
    
    Draw player to window surface

--- .github/Collision.py
+++ .github/Collision.py
@@ -1,4 +1,4 @@
-#Pygame Collision Detection Pratice, Brunswick Kyomari, Jan-027-22, 9:28am, 0.9
+#Pygame Collision Detection Pratice, Brunswick Kyomari, Jan-027-22, 9:36am, 1.0
 
 import pygame, sys, random
 from pygame.locals import
@@ -85,3 +85,16 @@ while True:
 
     # Draw white background on Window Surface.
     windowSurface.fill(WHITE)
+
+    # Move the player
+    if moveDown and player.bottom < WINDOWHEIGHT:
+        player.top += MOVESPEED
+    if moveUp and player.top > 0:
+        player.top -= MOVESPEED
+    if moveLeft and player.left >0:
+        player.left -= MOVESPEED
+    if moveRight and player.right < WINDOWWIDTH:
+        player.right += MOVESPEED
+
+    # Draw the player on the surface.
+    pygame.draw.rect(windowSurface, BLACK, player)
